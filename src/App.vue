c208c7e27096f9b88e5bb7cc6e53d215138f6a97
m0slah <muhammedslahqadir@gmail.com>
2023-07-05 08:16:21 +0300

    add transaction

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,34 @@
-<template></template>
+<template>
+  <button type="button" @click="flag = !flag">Toggle</button>
 
-<script></script>
+  <Transition name="fade">
+    <h2 v-if="flag">Hello World!</h2>
+  </Transition>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      flag: false,
+    };
+  },
+};
+</script>
+
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 2s linear;
+}
+
+.fade-leave-to{
+  transition: all 0.25s linear;
+  opacity: 0;
+}
+
+</style>
