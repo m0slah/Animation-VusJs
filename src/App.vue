0f049c6aeb9a35df814252bd799bdcbcbf13402d
m0slah <muhammedslahqadir@gmail.com>
2023-07-05 09:10:55 +0300

    animating with CSS animation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
 
-  <Transition name="fade" mode="out-in">
+  <!-- <Transition name="fade" mode="out-in">
     <h2 v-if="flag" key="main">Hello World!</h2>
     <h2 v-else key="secondary">another Hello!</h2>
-  </Transition>
+  </Transition> -->
+
+  <transition name="zoom" type="animation" appear>
+    <h2 v-if="flag">hello</h2>
+  </transition>
 </template>
 
 <script>
@@ -12,38 +16,73 @@ export default {
   name: "App",
   data() {
     return {
-      flag: false,
+      flag: true,
     };
   },
 };
 </script>
 
 <style>
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+
 .fade-enter-from {
   opacity: 0;
 }
 
 .fade-enter-active {
-  transition: all 0.5s linear;
+  transition: all 1s linear;
 }
 
-.fade-enter-to{
-  transition: all 0.5s linear;
+.fade-enter-to {
+  transition: all 1s linear;
   color: red;
 }
 
-.fade-leave-form{
-  transition: all 0.5s linear;
+.fade-leave-form {
+  transition: all 1s linear;
   background-color: aquamarine;
 }
 
-.fade-enter-active{
-  transition: all 0.3s;
+.fade-enter-active {
+  transition: all 1s;
   background-color: chartreuse;
 }
 
 .fade-leave-to {
-  transition: all 3s linear;
+  transition: all 1s linear;
   opacity: 0;
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 2s linear;
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 2s linear;
+
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
 }
 </style>
